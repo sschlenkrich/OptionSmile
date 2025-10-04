71265e27d3480109a5f586f50e59efe28398b97e
Sebastian Schlenkrich <sebastian.schlenkrich@frame-consult.de>
2025-10-04 10:45:36 +0200

    Add docker configuration via debian-slim

---
+++ docker/set_volume_paths.bat
@@ -0,0 +1,5 @@
+
+set data_dir="C:\Users\Sebastian\01_GitHub\sschlenkrich\OptionSmile\data"
+
+rem set dolt_dir="C:\Users\Sebastian\.dolt"
+set dolt_dir="C:\Users\Sebastian\01_GitHub\sschlenkrich\OptionSmile\docker\dolt"
